77a8c9be82b683f0fd9dbee20450a65bacf0eb9c
LaytonAvery <laytonavery5@gmail.com>
2021-02-26 23:28:39 -0500

    initial commit

---
+++ form.js
@@ -0,0 +1,7 @@
+let registrationForm = document.getElementById("registrationForm");
+let fName = document.getElementById("fName");
+let lName = document.getElementById("lName");
+let emailAdd = document.getElementById("email");
+let password = document.getElementById("password");
+
+registrationForm.setCustomValidity("Please enter this.");
